a8aac5142df7242e89b1103207634a107d6df0cf
FeedMyKids1 <47508141+KampinKarl1@users.noreply.github.com>
2025-08-17 15:51:31 +0200

    Update MeshDeformer.cs

--- MeshDeformer.cs
+++ MeshDeformer.cs
@@ -1,30 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
+using System;
+using System.Threading.Tasks;
 
 public class MeshDeformer : MonoBehaviour
 {
     public bool useDamage = true;                                               // Use Damage.
     private struct originalMeshVerts { public Vector3[] meshVerts; }    // Struct for Original Mesh Verticies positions.
     private originalMeshVerts[] originalMeshData;                           // Array for struct above.
-    /// <summary>
-    /// An 
-    /// </summary>
-    private Vector3[] originalMeshVertices;
+
     public MeshFilter[] deformableMeshFilters;                              // Deformable Meshes.
     [SerializeField] private MeshCollider meshCollider;
     public LayerMask damageFilter = -1;                                     // LayerMask filter for not taking any damage.
     public float randomizeVertices = 1f;                                            // Randomize Verticies on Collisions for more complex deforms.
     public float damageRadius = .5f;                                                // Verticies in this radius will be effected on collisions.
-    private float minimumVertDistanceForDamagedMesh = .002f;        // Comparing Original Vertex Positions Between Last Vertex Positions To Decide Mesh Is Repaired Or Not.
- 
+   
     public float maximumDamage = .5f;               // Maximum Vert Distance For Limiting Damage. 0 Value Will Disable The Limit.
     private float minimumCollisionForce = 5f;       // Minimum collision force.
-    public float damageMultiplier = 1f;             // Damage multiplier.
+    public float deformMultiplier = 1f;             // Damage multiplier.
 
     void Start() 
     {
+        if (deformableMeshFilters.Length == 0)
+            deformableMeshFilters = new MeshFilter[] { GetComponent<MeshFilter>() };
+        if (meshCollider == null)
+            meshCollider = GetComponent<MeshCollider>();
+
         InitOriginalMeshData();
     }
     private void InitOriginalMeshData()
@@ -40,11 +42,16 @@ public class MeshDeformer : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
         //NO COLLISION
-        if (collision.contacts.Length < 1) 
+        if (collision.contacts.Length < 1)
+        {
             return;
+        }
         //NOT ENOUGH FORCE
         if (collision.relativeVelocity.magnitude < minimumCollisionForce)
+        {
+            print("Escaped collision: First force check");
             return;
+        }
 
 
         Vector3 colRelVel = collision.relativeVelocity;
@@ -54,19 +61,40 @@ public class MeshDeformer : MonoBehaviour
 
         //RECHECK FORCE OF IMPACT GREAT ENOUGH
         if (colRelVel.magnitude * angleOfImpact < minimumCollisionForce)
+        {
+            print("Escaped collision: SECOND force check");
             return;
+        }
 
 
-        Vector3 localImpactVector = transform.InverseTransformDirection(colRelVel) * (damageMultiplier / 50f); //What does the 50 represent?
+        Vector3 localImpactVector = transform.InverseTransformDirection(colRelVel) * (deformMultiplier / 50f); //What does the 50 represent?
+
+
+        StartCoroutine(SynchronizedMeshDeformation(collision, angleOfImpact, localImpactVector));
+    }
+
+    private IEnumerator SynchronizedMeshDeformation(Collision collision, float angleOfImpact, Vector3 localImpactVector) 
+    {
+        yield return new WaitForEndOfFrame();
 
         for (int i = 0; i < deformableMeshFilters.Length; i++)
             DeformMesh(i, originalMeshData[i].meshVerts, collision, angleOfImpact, localImpactVector);
 
-            Destroy(meshCollider);
+
+
+
+        yield return new WaitForEndOfFrame();
+
+        ReplaceCollider();
+    }
+
+    private void ReplaceCollider() 
+    {
+        Destroy(meshCollider);
         meshCollider = gameObject.AddComponent<MeshCollider>();
     }
 
-    private void DeformMesh(int meshIndex, Vector3[] originalMesh, Collision collision, float cos, Vector3 localImpactVector)
+    private void DeformMesh(int meshIndex, Vector3[] originalMesh, Collision collision, float angleOfImpact, Vector3 localImpactVector)
     {
         Mesh mesh = deformableMeshFilters[meshIndex].mesh;
         Transform meshTransform = deformableMeshFilters[meshIndex].transform;
@@ -75,10 +103,36 @@ public class MeshDeformer : MonoBehaviour
 
         for (int i = 0; i < collision.contacts.Length; i++)
         {
-            ContactPoint contact = collision.contacts[i];
+            Vector3 contactPoint = collision.contacts[i].point;
 
-            Vector3 point = meshTransform.InverseTransformPoint(contact.point);
+            Vector3 point = meshTransform.InverseTransformPoint(contactPoint);
 
+            Parallel.For(0, vertices.Length, v => 
+            {
+                float vertDistanceFromImpact = (point - vertices[v]).magnitude;
+
+
+                if (vertDistanceFromImpact > damageRadius)
+                {
+                    return; //same as continue in Parallel.For
+                }
+
+
+                vertices[v] += Quaternion.LookRotation(collision.relativeVelocity)
+                    * ((localImpactVector * (damageRadius - (point - vertices[v]).magnitude) / damageRadius)
+                    * angleOfImpact
+                    + (new Vector3(
+                        Mathf.Sin(vertices[v].y),
+                        Mathf.Sin(vertices[v].z),
+                        Mathf.Sin(vertices[v].x)).normalized)
+                        * .002f);
+
+                if (maximumDamage > 0 && ((vertices[v] - originalMesh[v]).magnitude) > maximumDamage)
+                {
+                    vertices[v] = originalMesh[v] + (vertices[v] - originalMesh[v]).normalized * (maximumDamage);
+                }
+            });
+            /*
             for (int v = 0; v < vertices.Length; v++)
             {
 
@@ -86,16 +140,27 @@ public class MeshDeformer : MonoBehaviour
 
 
                 if (vertDistanceFromImpact > damageRadius)
+                {
+                    print("Escaped collision: Distance check");
                     continue;
+                }
 
 
-                vertices[v] += /*rot **/Quaternion.LookRotation( collision.relativeVelocity)* ((localImpactVector * (damageRadius - (point - vertices[v]).magnitude) / damageRadius) * cos + (new Vector3(Mathf.Sin(vertices[v].y * 1000), Mathf.Sin(vertices[v].z * 1000), Mathf.Sin(vertices[v].x * 100)).normalized * (randomizeVertices / 500f)));
+                vertices[v] += Quaternion.LookRotation(collision.relativeVelocity)
+                    * ((localImpactVector * (damageRadius - (point - vertices[v]).magnitude) / damageRadius)
+                    * angleOfImpact
+                    + (new Vector3(
+                        Mathf.Sin(vertices[v].y),
+                        Mathf.Sin(vertices[v].z),
+                        Mathf.Sin(vertices[v].x)).normalized) 
+                        * .002f);
+                
                 if (maximumDamage > 0 && ((vertices[v] - originalMesh[v]).magnitude) > maximumDamage)
                 {
                     vertices[v] = originalMesh[v] + (vertices[v] - originalMesh[v]).normalized * (maximumDamage);
                 }
 
-            }
+            }*/
 
         }
 
@@ -106,4 +171,3 @@ public class MeshDeformer : MonoBehaviour
         
     }
 }
-
